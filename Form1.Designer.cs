079d91d369139b38abec7818ec1689f75fda6619
N1trousOxide <nshagalov@gmail.com>
2022-09-17 17:33:54 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace PsychologicalTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartupLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartupLabel
+            // 
+            this.StartupLabel.AutoSize = true;
+            this.StartupLabel.Location = new System.Drawing.Point(396, 533);
+            this.StartupLabel.Name = "StartupLabel";
+            this.StartupLabel.Size = new System.Drawing.Size(204, 13);
+            this.StartupLabel.TabIndex = 0;
+            this.StartupLabel.Text = "Очень длинный текст для приветствия";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(459, 560);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Далее";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(122, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(794, 506);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 564);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(367, 560);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Обзор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 570);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Папка карточек";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(555, 569);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Папка фраз";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(642, 563);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(940, 559);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Обзор";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 647);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.StartupLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label StartupLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PsychologicalTest
+{
+    public partial class Form1 : Form
+    {   
+        
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click);
+            pictureBox1.Hide();
+            button1.Text = "Начать";
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label1.Hide();
+            label2.Hide();
+            button2.Hide();
+            button3.Hide();
+            button1.Text = "Далее";
+            string TextFile = textBox2.Text;
+            string[] Lines = File.ReadAllLines(TextFile);
+            Random rand = new Random();
+            int index = rand.Next(Lines.Length);
+            string RandomPhrase = Lines[index];
+            StartupLabel.Text = RandomPhrase;
+            button1.Click += new EventHandler(button1_secondClick);
+            textBox1.Hide();
+            textBox2.Hide();
+        }
+        public void button1_secondClick(object sender, EventArgs e)
+        {
+            StartupLabel.Hide();
+            string FolderPath = textBox1.Text;
+            textBox1.Hide();
+            textBox2.Hide();
+            string[] PicList1 = Directory.GetFiles(FolderPath, "*.jpg");
+            string[] PicList2 = Directory.GetFiles(FolderPath, "*.png");
+            string[] PicList = new string[PicList1.Length + PicList2.Length];
+            PicList1.CopyTo(PicList, 0);
+            PicList2.CopyTo(PicList, PicList1.Length);
+            pictureBox1.Show();
+            Random rand2 = new Random();
+            int index1 = rand2.Next(PicList.Length);
+            pictureBox1.Image = Image.FromFile(PicList[index1]);
+            button1.Click += new EventHandler(button1_thirdClick);
+        }
+
+        private void button1_thirdClick(object sender, EventArgs e)
+        {
+            pictureBox1.Hide();
+            StartupLabel.Show();
+            string TextFile = textBox2.Text;
+            string[] Lines = File.ReadAllLines(TextFile);
+            Random rand = new Random();
+            int index = rand.Next(Lines.Length);
+            string RandomPhrase = Lines[index];
+            StartupLabel.Text = RandomPhrase;
+            button1.Click += new EventHandler(button1_fourthClick);
+            textBox1.Hide();
+            textBox2.Hide();
+            
+        }
+
+        private void button1_fourthClick(object sender, EventArgs e)
+        {
+            pictureBox1.Show();
+            StartupLabel.Hide();
+            button1.Text = "Закрыть";
+            button1.Click += new EventHandler(button1_fifthClick);
+        }
+
+        private void button1_fifthClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult result = folderDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = folderDlg.SelectedPath;
+                Environment.SpecialFolder root = folderDlg.RootFolder;              
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDlg = new OpenFileDialog();
+            DialogResult result = fileDlg.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = fileDlg.FileName;
+            }
+        }
+    }
+}
